4e19bb46ef2bd93ce832571121252dc513a32976
Anton Bankevich <abankevich@bloom.ucsd.edu>
2019-10-04 17:04:10 -0700

    Fix

--- scripts/recruit.py
+++ scripts/recruit.py
@@ -3,11 +3,10 @@ import shutil
 import subprocess
 import sys
 
-from common.seq_records import NamedSequence
 
 sys.path.append("py")
 
-
+from common.seq_records import NamedSequence
 from common.SimpleGraph import SimpleGraph
 from common.sequences import ContigStorage, Contig
 from alignment.align_tools import DirDistributor, Aligner
@@ -28,22 +27,43 @@ def main(flye_dir, rf, dir, edge_id, k):
     for s in open(os.path.join(flye_dir, "flye.log"), "r").readlines():
         if s.find("UPath") != -1:
             s = s.split()
-            id_map[s[4][:-1]] = id_map[5::2]
-            id_map["-" + s[4][:-1]] = map(lambda val: basic.Reverse(val), id_map[5::2])
+            id_map[s[4][:-1]] = s[5::2]
+            id_map["-" + s[4][:-1]] = map(lambda val: basic.Reverse(val), s[5::2])
     edge_ids = edge_id.split(",")
     print "Extracting relevant graph component"
-    res = open(os.path.join(dir, "contigs.fasta"), "r")
+    res = open(os.path.join(dir, "contigs.fasta"), "w")
     unique = dict()
     for eid in edge_ids:
         for e in graph.v[graph.e[eid].start].inc:
+            if basic.Normalize(e.id) in edge_ids:
+                continue
             if len(e.seq) < 10000:
-                unique[e.id] = e
+                if e.id.startswith("-"):
+                    unique[e.id[1:]] = NamedSequence(basic.RC(e.seq), e.id[1:])
+                else:
+                    unique[e.id] = NamedSequence(e.seq, e.id)
             else:
                 if e.id.startswith("-"):
-                    unique[e.id + "l"] = NamedSequence(basic.RC(e.seq[:5000]), e.id + "l")
+                    unique[e.id[1:] + "l"] = NamedSequence(basic.RC(e.seq[:5000]), e.id[1:] + "l")
                 else:
                     unique[e.id + "r"] = NamedSequence(e.seq[-5000:], e.id + "r")
+        for e in graph.v[graph.e[eid].fin].out:
+            if basic.Normalize(e.id) in edge_ids:
+                continue
+            if len(e.seq) < 10000:
+                if e.id.startswith("-"):
+                    unique[e.id[1:]] = NamedSequence(basic.RC(e.seq), e.id[1:])
+                else:
+                    unique[e.id] = NamedSequence(e.seq, e.id)
+            else:
+                if e.id.startswith("-"):
+                    unique[e.id[1:] + "r"] = NamedSequence(basic.RC(e.seq[-5000:]), e.id[1:] + "r")
+                else:
+                    unique[e.id + "l"] = NamedSequence(e.seq[:5000], e.id + "l")
+
+
     for c in unique.values():
+        print c.id
         SeqIO.write(c, res, "fasta")
     res.close()
     old_ids = []
@@ -61,7 +81,7 @@ def main(flye_dir, rf, dir, edge_id, k):
             relevant_read_ids.add(s[2][1:])
             print s[2][1:], s[6].split("_")[1]
     print "Reading reads"
-    res = open(os.path.join(dir, "reads.fasta"), "r")
+    res = open(os.path.join(dir, "reads.fasta"), "w")
     for read in SeqIO.parse_fasta(open(rf, "r")):
         if read.id in relevant_read_ids and len(read) > k * 1.2:
             SeqIO.write(read, res, "fasta")
