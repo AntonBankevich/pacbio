554558b3935f04312ea2386955efee845c25d74a
Anton Bankevich <abankevich@bloom.ucsd.edu>
2019-11-18 16:29:30 -0800

    Fix

--- scripts/downsample.py
+++ scripts/downsample.py
@@ -1,12 +1,16 @@
 import sys
+sys.path.append("py")
 
 from common import SeqIO
 
 rf = sys.argv[1]
 outf = sys.argv[2]
 total = int(sys.argv[3])
+print "Reading reads"
 reads = list(SeqIO.parse_fasta(open(rf, "r")))
+print "Sorting reads"
 reads = sorted(reads, key = lambda read: -len(read))
+print "Printing reads"
 out = open(outf, "w")
 for read in reads:
     if total <= 0:
