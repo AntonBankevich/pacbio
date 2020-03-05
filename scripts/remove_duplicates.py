4cf77044fb6969f263ed7c9aee5c6eb18673e5b9
Anton Bankevich <anton.bankevich@gmail.com>
2020-03-04 18:54:01 -0800

    Duplicate reads removing script

---
+++ scripts/remove_duplicates.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("py")
+from common.sequences import ContigStorage
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+    contigs = ContigStorage().loadFromFasta(open(fname, "r"))
+    contigs.writeToFasta(sys.stdout)
