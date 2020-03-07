6806d75dfd17ffc61a70bd3b50bc645532289ffe
Anton Bankevich <anton.bankevich@gmail.com>
2020-03-06 21:09:16 -0800

    Fix

--- py/common/line_align.py
+++ py/common/line_align.py
@@ -148,7 +148,7 @@ class Scorer:
                 while len(dec) > 0 and dec[-1][0] <= vals[curr][0]:
                     dec.pop()
                 dec.append(vals[curr])
-            while curl < len(vals) and i > dec[0][1] and len(dec) > 1:
+            while curl < len(vals) and len(dec) > 1 and i > dec[0][1]:
                 dec.popleft()
                 curl += 1
             res.append(dec[0][0])
