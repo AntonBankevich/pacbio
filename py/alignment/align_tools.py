40aff2ae6fac3c9a15e8e38b23aebb49fec7d9ff
Anton Bankevich <abankevich@bloom.ucsd.edu>
2020-04-10 16:19:09 -0700

    Removed costly analysis during alignment

--- py/alignment/align_tools.py
+++ py/alignment/align_tools.py
@@ -342,14 +342,15 @@ if __name__ == "__main__":
     basic.CreateLog(dir)
     contigs = ContigCollection().loadFromFasta(open(target, "r"), False)
     for al in aln.localAlign(ReadCollection().loadFromFile(query), contigs):
+        sys.stdout.write(str(len(al)) + " ")
         sys.stdout.write(str(al))
         if len(al) > 40000:
             print ""
             continue
-        if len(al) > 5000:
-            for i in range(len(al.seg_from) / 1000):
-                seg = al.seg_from.prefix(length = i * 1000 + 1000).suffix(length = 1000)
-                sys.stdout.write(" " + str(al.reduce(query=seg).percentIdentity()))
+#        if len(al) > 5000:
+#            for i in range(len(al.seg_from) / 1000):
+#                seg = al.seg_from.prefix(length = i * 1000 + 1000).suffix(length = 1000)
+#                sys.stdout.write(" " + str(al.reduce(query=seg).percentIdentity()))
         sys.stdout.write("\n")
         s = list(al.asMatchingStrings())
         print s[0]
--- scripts/subsequences.py
+++ scripts/subsequences.py
@@ -8,10 +8,12 @@ from common.sequences import ContigStorage
 def printSegs(f, segs):
     c = ContigStorage().loadFromFasta(open(f, "r"), False)
     for seg in segs:
+        if seg[2] == 0:
+            seg[2] = len(c[seg[0]])
         SeqIO.write(c[seg[0]].segment(seg[1], seg[2]).asContig(), sys.stdout, "fasta")
         
 if __name__ == "__main__":
     segs = []
     for i in range(len(sys.argv) / 3):
-        segs.append((sys.argv[i * 3 + 2], int(sys.argv[i * 3 + 3]), int(sys.argv[i * 3 + 4])))
+        segs.append([sys.argv[i * 3 + 2], int(sys.argv[i * 3 + 3]), int(sys.argv[i * 3 + 4])])
     printSegs(sys.argv[1], segs)
