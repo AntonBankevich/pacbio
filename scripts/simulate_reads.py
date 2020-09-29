c627bc9fdca853003e856a0c11973f96ef30ecf7
Anton Bankevich <abankevich@bloom.ucsd.edu>
2020-09-29 16:22:59 -0700

    Simulate reads modification

--- scripts/simulate_reads.py
+++ scripts/simulate_reads.py
@@ -5,29 +5,25 @@ sys.path.append("py")
 from common import basic, SeqIO
 from common.sequences import ContigCollection
 
-def main(ref_file, contig_name, rlen, cov, dir):
-    contig_names = contig_name.split(",")
+def main(ref_file, contig_size, rlen, cov, dir):
     basic.ensure_dir_existance(dir)
     all_contigs = ContigCollection().loadFromFasta(open(ref_file, "r"), False)
-    for contig_name in contig_names:
-        if contig_name not in all_contigs.items.keys():
-            print "Fail", contig_name
+    contig_file_name = os.path.join(dir, "contigs.fasta")
+    contig_file = open(contig_file_name, "w")
+    reads_file_name = os.path.join(dir, "reads.fasta")
+    reads_file = open(reads_file_name, "w")
+    for ref in all_contigs.unique():
+        if len(ref) < contig_size:
             continue
-        ref = all_contigs[contig_name]
-        contig_file_name = os.path.join(dir, contig_name + ".fasta")
-        contig_file = open(contig_file_name, "w")
         SeqIO.write(ref, contig_file, "fasta")
-        contig_file.close()
-
-        reads_file_name = os.path.join(dir, contig_name + "_reads.fasta")
-        reads_file = open(reads_file_name, "w")
         for i in range(0, len(ref), max(1, rlen / cov)):
             read = ref.segment(i, min(i + rlen, len(ref))).asNamedSequence()
             SeqIO.write(read, reads_file, "fasta")
-        reads_file.close()
-        print "Done", contig_name
-        print contig_file_name
-        print reads_file_name
+    reads_file.close()
+    contig_file.close()
+    print "Done"
+    print contig_file_name
+    print reads_file_name
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), sys.argv[5])
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), sys.argv[5])
