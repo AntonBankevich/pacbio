332fa93b35cb46eccca738becad3eb31642c1d1a
Anton bankevich <anton.bankevich@gmail.com>
2020-10-15 19:19:50 -0700

    bad reads are always printed

--- py/disjointig_resolve/initialization.py
+++ py/disjointig_resolve/initialization.py
@@ -134,12 +134,12 @@ def CreateDisjointigCollection(d_files, dir, aligner, reads):
         if not al.contradictingRTC(al.seg_to.contig.asSegment(), params.bad_end_length) and len(al.seg_from.contig) > len(al) - 2 * params.bad_end_length:
             good_reads.add(al.seg_from.contig.id)
     sys.stdout.info("Fraction of reads without full alignment to disjointigs:", 1 - float(len(good_reads)) / len(reads))
-    if len(good_reads) > 0.99 * len(bad_reads):
+    rf = os.path.join(dir, "badreads.fasta")
+    bad_reads = bad_reads.filter(lambda read: read.id not in good_reads)
+    if len(good_reads) > 100 * len(bad_reads):
         sys.stdout.info("Alomst all reads have good alignments. Skipping disjointig collection extension.")
         disjointigs.addAlignments(aligner.localAlign(reads, disjointigs))
         return disjointigs
-    rf = os.path.join(dir, "badreads.fasta")
-    bad_reads = bad_reads.filter(lambda read: read.id not in good_reads)
     tlen = sum(map(len, bad_reads))
     bad_reads.print_fasta(open(rf, "w"))
     l = tlen * clen / tlen0
